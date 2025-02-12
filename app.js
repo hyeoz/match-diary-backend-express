304e1759f43ba517a8b43d52a1884ee6628b012f
hyeoz <jenny970203@gmail.com>
2025-02-12 22:46:22 +0900

    fix: port again

--- app.js
+++ app.js
@@ -285,6 +285,6 @@ app.use((err, req, res, next) => {
 });
 
 // ANCHOR SERVER
-app.listen(80, () => {
-  console.log("Server is running on port 80");
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
 });
